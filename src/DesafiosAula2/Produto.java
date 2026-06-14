813dffd117e7e1a189a981172d4ac49b56307bbd
Gustavo <gbonatto19@gmail.com>
2026-06-13 23:34:27 -0300

    Produto x Quantidade

--- src/DesafiosAula2/Produto.java
+++ src/DesafiosAula2/Produto.java
@@ -1,2 +1,10 @@
-package DesafiosAula2;public class Produto {
+package DesafiosAula2;
+
+public class Produto {
+    public static void main(String[] args) {
+        double precoProduto = 259.99;
+        int quantidade = 9;
+        int valorTotal = (int) (precoProduto * quantidade);
+        System.out.println(valorTotal);
+    }
 }
