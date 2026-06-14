39b67986773ea41ae505f2b933c7bd72e4d60b37
Gustavo <gbonatto19@gmail.com>
2026-06-14 00:34:00 -0300

    Número é Par ou Ímpar

--- src/DesafiosAula3/ParOuImpar.java
+++ src/DesafiosAula3/ParOuImpar.java
@@ -1,2 +1,20 @@
-package DesafiosAula3;public class ParOuImpar {
+package DesafiosAula3;
+//Classe que diz se é par ou impar
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = leitura.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é ímpar.");
+        }
+    }
 }
+
