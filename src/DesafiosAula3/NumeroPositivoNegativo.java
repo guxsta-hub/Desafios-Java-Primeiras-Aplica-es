bd9eba0bbec015c1ac653cc03e8e2e3180565bb5
Gustavo <gbonatto19@gmail.com>
2026-06-14 00:34:00 -0300

    Número é positivo ou negativo

--- src/DesafiosAula3/NumeroPositivoNegativo.java
+++ src/DesafiosAula3/NumeroPositivoNegativo.java
@@ -1,2 +1,20 @@
-package DesafiosAula3;public class NumeroPositivoNegativo {
+package DesafiosAula3;
+//Classe que diz se é positivo ou negativo
+import java.util.Scanner;
+
+public class NumeroPositivoNegativo {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        double numero = leitura.nextDouble();
+
+        if (numero > 0) {
+            System.out.println("Número positivo");
+        } else {
+            System.out.println("Número negativo");
+        }
+    }
 }
+
