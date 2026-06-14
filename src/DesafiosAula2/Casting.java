17033f54dc3fdfa34fd0ebff02b1a1623c87bdfd
Gustavo <gbonatto19@gmail.com>
2026-06-13 23:34:08 -0300

    Casting de números

--- src/DesafiosAula2/Casting.java
+++ src/DesafiosAula2/Casting.java
@@ -1,2 +1,10 @@
-package DesafiosAula2;public class Casting {
+package DesafiosAula2;
+
+public class Casting {
+    public static void main(String[] args) {
+        double n1 = 95.50;
+        double n2 = 150.90;
+        int casting = (int) (n1 + n2);
+        System.out.println(casting);
+    }
 }
