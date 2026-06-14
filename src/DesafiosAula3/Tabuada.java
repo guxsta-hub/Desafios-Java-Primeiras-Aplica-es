537ae823ca6ba76838731bef9d8fe551e96f3523
Gustavo <gbonatto19@gmail.com>
2026-06-14 00:34:00 -0300

    Tabuada de um número

--- src/DesafiosAula3/Tabuada.java
+++ src/DesafiosAula3/Tabuada.java
@@ -1,2 +1,17 @@
-package DesafiosAula3;public class Tabuada {
-}
+package DesafiosAula3;
+//Classe que faz a tabuada
+import java.util.Scanner;
+
+public class Tabuada {
+        public static void main(String[] args) {
+
+            Scanner leitura = new Scanner(System.in);
+
+            System.out.print("Digite um número: ");
+            int numero = leitura.nextInt();
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero + " x " + i + " = " + (numero * i));
+            }
+        }
+    }
