455a90f749893f7458b71347de4078a2bae25a07
Gustavo <gbonatto19@gmail.com>
2026-06-13 23:34:35 -0300

    Conversão Real para Dólar

--- src/DesafiosAula2/RealDolares.java
+++ src/DesafiosAula2/RealDolares.java
@@ -1,2 +1,12 @@
-package DesafiosAula2;public class RealDolares {
+package DesafiosAula2;
+
+public class RealDolares {
+    public static void main(String[] args) {
+        double valorEmDolares = 10.0;
+        double cotacaoDolar = 4.94;
+
+        double valorEmReais = valorEmDolares * cotacaoDolar;
+
+        System.out.println(valorEmReais);
+    }
 }
