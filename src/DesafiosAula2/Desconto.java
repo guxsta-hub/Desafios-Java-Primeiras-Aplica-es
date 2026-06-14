70275ef589d459c4b5af809f4bf9430e31781408
Gustavo <gbonatto19@gmail.com>
2026-06-13 23:34:43 -0300

    Cálculo de desconto

--- src/DesafiosAula2/Desconto.java
+++ src/DesafiosAula2/Desconto.java
@@ -1,2 +1,15 @@
-package DesafiosAula2;public class Desconto {
+package DesafiosAula2;
+
+public class Desconto {
+    public static void main(String[] args) {
+        double precoOriginal = 100.0;
+        double percentualDesconto = 10.0;
+
+        double valorDesconto = precoOriginal * percentualDesconto / 100;
+        double precoComDesconto = precoOriginal - valorDesconto;
+
+        System.out.println("Preço original: R$ " + precoOriginal);
+        System.out.println("Desconto: R$ " + valorDesconto);
+        System.out.println("Preço com desconto: R$ " + precoComDesconto);
+    }
 }
