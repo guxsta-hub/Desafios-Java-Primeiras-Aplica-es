6977ae1bcb1908688aff06b63cf0b1012a2fc303
Gustavo <gbonatto19@gmail.com>
2026-06-13 23:08:42 -0300

    Adicionando descrições nas classes

--- src/DesafiosAula1/Soma.java
+++ src/DesafiosAula1/Soma.java
@@ -1,5 +1,5 @@
 package DesafiosAula1;
-
+// Classe que realiza soma
 public class Soma {
     public static void main(String[] args) {
         System.out.println(10 + 20);
--- src/DesafiosAula1/Subtracao.java
+++ src/DesafiosAula1/Subtracao.java
@@ -1,5 +1,5 @@
 package DesafiosAula1;
-
+// Classe que realiza subtração
 public class Subtracao {
     public static void main(String[] args) {
         System.out.println(5 * 10);
