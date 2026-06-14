a34c85b8bc72adaeb836f80a99d42636b360e6b5
Gustavo <gbonatto19@gmail.com>
2026-06-14 00:34:00 -0300

    Comparação maior e menor

--- src/DesafiosAula3/CompararNumeros.java
+++ src/DesafiosAula3/CompararNumeros.java
@@ -1,2 +1,25 @@
-package DesafiosAula3;public class CompararNumeros {
+package DesafiosAula3;
+//Classe que faz comparação de números
+import java.util.Scanner;
+
+public class CompararNumeros {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = leitura.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int numero2 = leitura.nextInt();
+
+        if (numero1 == numero2) {
+            System.out.println("Os números são iguais.");
+        } else if (numero1 > numero2) {
+            System.out.println("O primeiro número é maior.");
+        } else {
+            System.out.println("O segundo número é maior.");
+        }
+    }
 }
+
