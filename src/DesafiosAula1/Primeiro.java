14694c16ee06e588f9ab18a4e62c16734758882a
Gustavo <gbonatto19@gmail.com>
2026-06-13 22:01:02 -0300

    Desafios aula1

---
+++ src/DesafiosAula1/Primeiro.java
@@ -0,0 +1,7 @@
+package DesafiosAula1;
+
+public class Primeiro {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/DesafiosAula1/Subtracao.java
@@ -0,0 +1,7 @@
+package DesafiosAula1;
+
+public class Subtracao {
+    public static void main(String[] args) {
+        System.out.println(5 * 10);
+    }
+}
