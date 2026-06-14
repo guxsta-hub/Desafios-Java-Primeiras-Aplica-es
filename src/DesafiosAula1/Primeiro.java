34fdc3b95de0ba8af749a51685f6d2005a95cc26
Gustavo <gbonatto19@gmail.com>
2026-06-13 23:10:43 -0300

    Primeiro exercício da aula 1

--- src/DesafiosAula1/Primeiro.java
+++ src/DesafiosAula1/Primeiro.java
@@ -1,5 +1,5 @@
 package DesafiosAula1;
-// Meu primeiro código
+// Meu primeiro código.
 public class Primeiro {
     public static void main(String[] args) {
         System.out.println("Hello world!");
